7d748833bc03d6d249d3e93d0aff4247f4e2ddb8
Helio Gonzales <heliogonzalesr@gmail.com>
2020-12-28 12:11:45 +0100

    repeti clases del DOM

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,12 @@
-console.log(window);
+/* console.log(window);
 console.log(document);
 
 let texto = "Hola soy Helio Gonzales y estoy estudiando JavaScript";
 const hablar = (texto) => 
 speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
-hablar(texto);
+hablar(texto); */
 
-console.log(window.document);
+/* console.log(window.document);
 console.log(document);
 console.log(document.head);
 console.log(document.body);
@@ -22,4 +22,167 @@ console.log(document.scripts);
 setTimeout(() => {
   console.log(document.getSelection().toString());
 }, 2000);
-document.write("<h2>Hola Mundo desde el DOM</h2>");
+document.write("<h2>Hola Mundo desde el DOM</h2>"); */
+
+
+/* console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementById("menu"));
+console.log(document.querySelector("#menu"));
+console.log(document.querySelector("a"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("a").length);document.querySelectorAll("a").forEach((el) => console.log(el));
+console.log(document.querySelector(".card"));
+console.log(document.querySelectorAll(".card"));
+console.log(document.querySelectorAll(".card")[2]);
+console.log(document.querySelectorAll("#menu li"));
+console.log(document.querySelector("#menu li")); */
+
+
+
+
+
+/* console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+console.log(document.querySelector(".link-dom").href);
+console.log(document.querySelector(".link-dom").getAttribute("href"));
+
+document.documentElement.lang = "es";
+console.log(document.documentElement.lang);
+document.documentElement.setAttribute("lang", "en");
+console.log(document.documentElement.lang);
+
+const $linkDom = document.querySelector(".link-dom");
+
+$linkDom.setAttribute("target", "_blank");
+$linkDom.setAttribute("rel", "noopener");
+$linkDom.setAttribute("href", "https://www.youtube.com/");
+
+console.log($linkDom.hasAttribute("rel"));
+$linkDom.removeAttribute("rel");
+console.log($linkDom.hasAttribute("rel"));
+
+// Data Attributes
+
+console.log($linkDom.getAttribute("data-description"));
+console.log($linkDom.dataset);
+console.log($linkDom.dataset.description);
+$linkDom.setAttribute("data-description", "Modelo de Objeto del Documento");
+console.log($linkDom.dataset.description);
+$linkDom.dataset.description = "Suscribete a mi canal y comparte";
+console.log($linkDom.dataset.description);
+console.log($linkDom.hasAttribute("data-id"));
+console.log($linkDom.removeAttribute("data-id"));
+console.log($linkDom.hasAttribute("data-id")); */
+
+
+
+
+
+/* const $linkDom = document.querySelector(".link-dom");
+
+console.log($linkDom.style);
+console.log($linkDom.getAttribute("style"));
+console.log($linkDom.style.backgroundColor);
+console.log($linkDom.style.color);
+console.log(window.getComputedStyle($linkDom));
+console.log(getComputedStyle($linkDom).getPropertyValue("color"));
+
+$linkDom.style.setProperty("text-decoration", "none");
+$linkDom.style.setProperty("display", "block");
+$linkDom.style.width = "50%";
+$linkDom.style.textAlign = "center";
+$linkDom.style.marginLeft = "auto";
+$linkDom.style.marginRight = "auto";
+$linkDom.style.padding = "1rem";
+$linkDom.style.borderRadius = "1.5rem";
+
+console.log($linkDom.style);
+console.log($linkDom.getAttribute("style"));
+console.log(getComputedStyle($linkDom));
+
+
+// Variables CSS - Custom Properties CSS
+const $html = document.documentElement,
+  $body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
+  varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+  
+console.log(varDarkColor, varYellowColor);
+
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+$html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+
+$body.style.setProperty("background-color", varDarkColor); */
+
+
+
+
+
+
+/* const $card = document.querySelector(".card");
+
+console.log($card);
+console.log($card.className);
+console.log($card.classList);
+console.log($card.classList.contains("rotate-45"));
+$card.classList.add("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+console.log($card.className);
+console.log($card.classList);
+$card.classList.remove("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.replace("rotate-45", "rotate-135");
+$card.classList.add("opacity-80", "sepia");
+$card.classList.remove("opacity-80", "sepia");
+$card.classList.toggle("opacity-80", "sepia"); */
+
+
+
+
+/* const $whatIsDOM = document.getElementById("que-es");
+
+let text = `
+  <p>
+   El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model</i></b>) es un API para documentos HTML y XML.
+  </p>
+  <p>
+   Este provee una representacion estructural del docuento, permitiendo modificar su contenido y presentacion visual mediante codigo JS.
+  </p>
+  <p>
+   <mark>El DOM no es parte de la especificacion de JavaScript, es una API para los navegadores.</mark>
+  </p> 
+`;
+
+// $whatIsDOM.innerText = text;
+$whatIsDOM.textContent = text;
+$whatIsDOM.innerHTML = text;
+$whatIsDOM.outerHTML = text; */
+
+
+
+
+const $cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousElementSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest("div"));
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("section"));
