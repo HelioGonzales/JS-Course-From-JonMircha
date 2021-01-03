11f84870143769a8844869e68c5c454f8d400870
Helio Gonzales <heliogonzalesr@gmail.com>
2021-01-03 17:26:20 +0100

    Alarm

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,9 +1,10 @@
 import burgerMenu from "./dom/burger_menu.js";
-import {digitalClock, alaram} from "./dom/watch.js";
+import {digitalClock, alarm} from "./dom/watch.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     burgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+    alarm("assets/graciosos-alarma-es-tu-mujer-.mp3", "#activar-alarma", "#desactivar-alarma");
 });
