c2ecab82543c82fb6a4a7993fb8c692f1960b6a0
Helio Gonzales <heliogonzalesr@gmail.com>
2021-01-03 16:58:53 +0100

    Digital Watch

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,9 @@
 import burgerMenu from "./dom/burger_menu.js";
+import {digitalClock, alaram} from "./dom/watch.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     burgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
 });
