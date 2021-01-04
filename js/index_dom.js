eb9bcc32bc786bf4177a0fb7e35edc916a21afc2
Helio Gonzales <heliogonzalesr@gmail.com>
2021-01-04 17:33:06 +0100

    Cuenta regresiva

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,8 @@
 import burgerMenu from "./dom/burger_menu.js";
 import {digitalClock, alarm} from "./dom/watch.js";
 import {moveBall, shortcuts} from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
+
 
 const d = document;
 
@@ -8,6 +10,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     burgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/graciosos-alarma-es-tu-mujer-.mp3", "#activar-alarma", "#desactivar-alarma");
+    countdown("countdown", "January 04, 2021 15:44:30", "Feliz Cumpleaños Helio 👽");
 });
 
 d.addEventListener("keydown", (e) => {
