6c0a732e2b3b60ac5ca3e16f815e1619bb11b4b5
Helio Gonzales <heliogonzalesr@gmail.com>
2021-01-05 08:25:09 +0100

    Boton de Scroll

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,6 +2,7 @@ import burgerMenu from "./dom/burger_menu.js";
 import {digitalClock, alarm} from "./dom/watch.js";
 import {moveBall, shortcuts} from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
+import scrollTopButton from "./dom/boton_scroll.js";
 
 
 const d = document;
@@ -11,6 +12,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/graciosos-alarma-es-tu-mujer-.mp3", "#activar-alarma", "#desactivar-alarma");
     countdown("countdown", "January 04, 2021 15:44:30", "Feliz Cumpleaños Helio 👽");
+    scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
