e65d2ff7f512a612b3d58df208dd01357a0844e7
Helio Gonzales Rivas <heliogonzalesr@gmail.com>
2021-01-26 12:21:16 +0100

    Formularios

--- js/index_dom.js
+++ js/index_dom.js
@@ -15,6 +15,7 @@ import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
 import smartVideo from "./dom/video_inteligente.js";
+import contactFormValidations from "./dom/validaciones_formularios.js";
 
 
 const d = document;
@@ -36,6 +37,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     slider();
     scrollSpy();
     smartVideo();
+    contactFormValidations();
 });
 
 d.addEventListener("keydown", (e) => {
