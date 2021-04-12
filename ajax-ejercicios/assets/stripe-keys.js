f712c5b2912a44503db9c930f495a35db667316b
Helio Gonzales Rivas <heliogonzalesr@gmail.com>
2021-04-12 15:27:06 +0200

    eCommerce with stripe and added env files and gitignore

---
+++ ajax-ejercicios/assets/stripe-keys.js
@@ -0,0 +1,4 @@
+export default {
+  public: "pk_test_51IeMqXGL2N3oBDbZj3eJJZWsnmy4wJDiYQlUfzmjpXTr3ze4e1xnzkbfM4QV2g7cKlLdDivQWmFsHPTy5hWwvJVj00wh9FvPoQ",
+  secret: "sk_test_51IeMqXGL2N3oBDbZfCyAH3IsL40t2ma1S1bAqQsO4V8TcS58WCtVItxg9sCn1PcoZwKaGiTqJQtTAXooZhQgvmL500DGNlL4Uw"
+}
