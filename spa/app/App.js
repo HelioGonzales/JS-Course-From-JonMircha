87385a8fc1c94be5ea9ed6fb8315e3868fd563b1
Helio Gonzales Rivas <heliogonzalesr@gmail.com>
2021-04-24 18:08:32 +0200

    spa postCards

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,12 +1,27 @@
 import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
-import { Title } from "./components/Title.js";
+import { Header } from "./components/Header.js";
 import { Loader } from "./components/Loader.js";
+import { Post } from "./components/Posts.js";
+import { PostCard } from "./components/PostCard.js";
 
 export function App() {
   const d = document,
     $root = d.getElementById("root");
 
-  $root.appendChild(Title());
+  $root.appendChild(Header());
+  $root.appendChild(Post());
   $root.appendChild(Loader());
+
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      console.log(posts);
+
+      let html = "";
+      posts.forEach((post) => (html += PostCard(post)));
+      d.querySelector(".loader").style.display = "none";
+      d.getElementById("posts").innerHTML = html;
+    },
+  });
 }
---
+++ spa/app/components/Posts.js
@@ -0,0 +1,6 @@
+export function Post() {
+  const $posts = document.createElement("section");
+  $posts.id = "posts";
+  $posts.classList.add("grid-fluid");
+  return $posts;
+}
