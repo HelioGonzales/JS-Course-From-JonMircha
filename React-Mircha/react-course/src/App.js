63a849be000b59bec335096a517c870b333a630c
Helio Gonzales Rivas <heliogonzalesr@gmail.com>
2021-05-13 07:07:23 +0200

    second commit

--- React-Mircha/react-course/src/App.js
+++ React-Mircha/react-course/src/App.js
@@ -1,4 +1,7 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
+import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
 import "./App.css";
 
 function App() {
@@ -8,20 +11,38 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Starting React</p>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>Starting React</p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg="Hola soy un componente funcional expresado desde una prop" />
+          <hr />
+          <Propiedades
+            cadena="Esto es una cadena de texto"
+            numero={33}
+            booleano={true}
+            arreglo={[1, 2, 3]}
+            objeto={{ nombre: "Helio", correo: "heliogonzalesr@gmail.com" }}
+            funcion={(num) => num * num}
+            elementoReact={<i>Esto es un elemento React</i>}
+            componenteReact={
+              <Componente msg="Soy un componente pasado como Prop" />
+            }
+          />
+        </section>
       </header>
     </div>
   );
