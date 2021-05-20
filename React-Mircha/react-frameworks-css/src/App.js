6edc9bbd75c7cd82cc2e5199dc314c662ea20c88
Helio Gonzales Rivas <heliogonzalesr@gmail.com>
2021-05-20 10:46:49 +0200

    first commit

---
+++ React-Mircha/react-frameworks-css/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Bootstrap from "./components/Bootstrap";
+import ReactBootstrap from "./components/ReactBootstrap";
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      <ReactBootstrap />
+    </div>
+  );
+}
+
+export default App;
